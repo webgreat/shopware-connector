e6856aa7269856355cf6ea7499bfcb79c2dea6e9
root <root@jtldev>
2013-12-02 10:18:46 +0100

    Bootstrap + first Controller

---
+++ Bootstrap.php
@@ -0,0 +1,68 @@
+<?php
+class Shopware_Plugins_Frontend_Jtlconnector_Bootstrap extends Shopware_Components_Plugin_Bootstrap
+{
+    public function getCapabilities()
+    {
+        return array(
+            'install' => true,
+            'update' => false,
+            'enable' => true,
+        );
+    }
+
+    public function getLabel()
+    {
+        return 'JTL Shopware Connector';
+    }
+ 
+    public function getVersion()
+    {
+        return '1.0.0';
+    }
+ 
+    public function getInfo()
+    {
+        return array(
+            'version' => $this->getVersion(),
+            'label' => $this->getLabel(),
+            'author' => 'JTL-Software GmbH',
+            'description' => '',
+            'support' => 'JTL-Software Forum',
+            'link' => 'http://forum.jtl-software.de'
+        );
+    }
+ 
+    public function install()
+    {
+        $this->subscribeEvent(
+            'Enlight_Controller_Dispatcher_ControllerPath_Frontend_Jtlconnector',
+            'onGetControllerPathFrontend'
+        );
+
+        return array(
+    		'success' => true,
+    		'invalidateCache' => array('backend', 'proxy')
+        );
+	}
+
+    public function enable()
+    {
+        return true;
+    }
+
+    public function disable()
+    {
+        return true;
+    }
+
+    public function uninstall()
+    {
+        return true;
+    }
+
+    public static function onGetControllerPathFrontend(Enlight_Event_EventArgs $args)
+    {
+        return dirname(__FILE__) . '/Controllers/Frontend/Connector.php';
+    }
+}
+?>
---
+++ Controllers/Frontend/Connector.php
@@ -0,0 +1,16 @@
+<?php
+class Shopware_Controllers_Frontend_Jtlconnector extends Enlight_Controller_Action
+{
+    public function preDispatch()
+    {
+        if(in_array($this->Request()->getActionName(), array('index'))) {
+            Shopware()->Plugins()->Controller()->ViewRenderer()->setNoRender();
+        }
+    }
+
+    public function indexAction()
+    {
+        echo "huhu";
+    }
+}
+
