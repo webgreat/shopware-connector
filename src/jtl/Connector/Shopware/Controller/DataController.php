3ae1012b33c0a3a1dd1a2cf4aaf67829776656fa
Daniel Böhmer <daniel.boehmer@jtl-software.de>
2013-12-03 15:50:21 +0100

    First try Phase 1 Alpha 2

--- src/jtl/Connector/Shopware/Controller/DataController.php
+++ src/jtl/Connector/Shopware/Controller/DataController.php
@@ -6,11 +6,48 @@
 
 namespace jtl\Connector\Shopware\Controller;
 
+use \Shopware\Components\Api\Manager as ShopwareManager;
+
 /**
  * Product Controller
  * @access public
  */
 abstract class DataController
 {
-    
+    /**
+     * Add Item to Container
+     * 
+     * @param \jtl\Connector\ModelContainer\CoreContainer $container
+     * @param string $type
+     * @param multiple: mixed $kvs
+     * @param multiple: mixed $members
+     */
+    protected function addContainerPos(CoreContainer &$container, $type, $swType, array $kvs = null, array $subItems = null)
+    {
+        if (isset($container->items[$type][0])) {
+            $class = $container->items[$type][0];
+            $resource = ShopwareManager::getResource($swType);
+            $objs = $resource->getList();
+
+            if ($objs !== null && is_array($objs)) {
+                $config = $this->getConfig();
+                foreach ($objs as $obj) {                    
+                    $model = Mmc::getModel($class);
+                    $model->map(true, $obj);
+
+                    // Sub Item
+                    if ($subItems !== null) {
+                        foreach ($subItems as $subType => $members) {
+                            $member = $members[0];
+                            $subKvs = array($members[1] => $model->{$member});
+
+                            $this->addContainerPos($container, $subType, $subKvs);
+                        }
+                    }
+
+                    $container->add($type, $model->getPublic(array("_fields", "_isEncrypted")), false);
+                }
+            }
+        }
+    }
 }
--- src/jtl/Connector/Shopware/Model/DataModel.php
+++ src/jtl/Connector/Shopware/Model/DataModel.php
@@ -30,47 +30,29 @@ class DataModel
         foreach ($original->getFields() as $platformField => $connectorField) {
             if ($toConnector) {
                 if (strlen($platformField) > 0 && isset($obj->$platformField)) {
-                    if (LanguageUtil::check($connectorField, $platformField)) {
-                        $original->$connectorField = utf8_encode(LanguageUtil::map(null, $obj->$platformField));
-                    }
-                    else if (BooleanUtil::check($connectorField)) {
-                        $original->$connectorField = BooleanUtil::map($obj->$platformField);
-                    }
-                    else if (DateUtil::check($platformField)) {
-                        $original->$connectorField = DateUtil::map($obj->$platformField, null, $platformField);
+                    if (DateUtil::check($obj->$platformField)) {
+                        $original->$connectorField = DateUtil::map($obj->$platformField);
                     }
                     else {
-                        if (is_numeric($obj->$platformField)) {
-                            $original->$connectorField = $obj->$platformField;
-                        }
-                        else if ($obj->$platformField === null) {
+                        if ($obj->$platformField === null) {
                             $original->$connectorField = null;
                         }
                         else {
-                            $original->$connectorField = utf8_encode($obj->$platformField);
+                            $original->$connectorField = $obj->$platformField;
                         }
                     }
                 }
             }
             else if (strlen($platformField) > 0) {
-                if (LanguageUtil::check($connectorField, $platformField)) {
-                    $obj->$platformField = LanguageUtil::map($original->$connectorField);
-                }
-                else if (BooleanUtil::check($connectorField)) {
-                    $obj->$platformField = BooleanUtil::map(null, $platformField, $original->$connectorField);
-                }
-                else if (DateUtil::check($platformField)) {
-                    $obj->$platformField = DateUtil::map(null, $original->$connectorField, $platformField);
+                if (DateUtil::check($platformField)) {
+                    $obj->$platformField = DateUtil::map(null, $original->$connectorField);
                 }
                 else {
-                    if (is_numeric($original->$connectorField)) {
-                        $obj->$platformField = $original->$connectorField;
-                    }
-                    else if ($original->$connectorField === null) {
+                    if ($original->$connectorField === null) {
                         $obj->$platformField = null;
                     }
                     else {
-                        $obj->$platformField = utf8_decode($original->$connectorField);
+                        $obj->$platformField = $original->$connectorField;
                     }
                 }
             }
